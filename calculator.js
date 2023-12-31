f3cbcc68f5776fbe42f559adea03ee0c4f67f494
benjamintesfhun <155310636+benjamintesfhun@users.noreply.github.com>
2023-12-31 20:36:40 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let display;
+display="";
+display=document.getElementById("display-number-id")
+console.log(display)
+let acInput;
+acInput="";
+acInput=document.getElementById("btn-ac-id");
+console.log(acInput)
+let acInputValue;
+acInputValue="";
+acInputValue=acInput.value;
+let sevenInput;
+sevenInput="";
+sevenInput=document.getElementById("btn-seven-id");
+let sevenInputValue;
+sevenInputValue="";
+sevenInputValue=sevenInput.value;
+console.log(sevenInputValue);
+let nineInput ;
+nineInput="";
+nineInput=document.getElementById("btn-nine-id")
+let nineInputValue ;
+nineInputValue="";
+nineInputValue=nineInput.value;
+console.log(nineInputValue);
+
+
+
+
+
+
